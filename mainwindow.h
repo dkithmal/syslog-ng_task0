c90737d9bdcc928861ba6b8b91427b1be4596339
damitha kithmal <dkithmalfit@gmail.com>
2015-03-28 22:37:34 +0530

    license statements add

--- mainwindow.h
+++ mainwindow.h
@@ -1,3 +1,19 @@
+/*
+    Copyright (C) 2015 by Damitha Kithmal <dkithmalfit@gmail.com>
+    This file is part of GSOC 2015 syslog-ng task0
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the COPYING file for more details.
+*/
+
+
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
